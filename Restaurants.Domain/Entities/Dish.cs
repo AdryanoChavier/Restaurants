41e1b11e62c96ae3470576b82d54f27e5d7a5428
adryano.chavier <adryano.chavier@senac.br>
2025-03-25 14:27:14 -0300

    Adicionar arquivos de projeto.

---
+++ Restaurants.Domain/Entities/Dish.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace Restaurants.Domain.Entities;
+
+public class Dish
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+}
