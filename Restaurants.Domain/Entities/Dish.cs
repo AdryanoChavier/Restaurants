55f16828c617d49ba8519838df61e2d17e4d1b20
adryano.chavier <adryano.chavier@senac.br>
2025-03-25 17:23:01 -0300

    Adicionando KiloCalories no banco

--- Restaurants.Domain/Entities/Dish.cs
+++ Restaurants.Domain/Entities/Dish.cs
@@ -8,6 +8,7 @@ public class Dish
     public string? Name { get; set; }
     public string? Description { get; set; }
     public decimal Price { get; set; }
+    public int? KiloCalories { get; set; }
     
     public int RestaurantId { get; set; }
 }
